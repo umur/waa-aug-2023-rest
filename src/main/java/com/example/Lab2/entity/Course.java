e59fdb450a8131076c0316b92270328a94ea0200
Kalkidan Mesfin <mesfinkalkidan@gmail.com>
2023-07-25 14:35:56 -0500

    student and course repository

---
+++ src/main/java/com/example/Lab2/entity/Course.java
@@ -0,0 +1,12 @@
+package com.example.Lab2.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Course {
+    private int id;
+    private String name;
+    private String code;
+}
---
+++ src/main/java/com/example/Lab2/entity/Student.java
@@ -0,0 +1,16 @@
+package com.example.Lab2.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+public class Student {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String major;
+    private List<Course> courses;
+}

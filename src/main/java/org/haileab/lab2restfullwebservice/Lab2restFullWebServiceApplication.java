2189ce1c705e28fe0e83bb1e3a936c08cc272f92
Haileab-TG <haileab.t.g@gmail.com>
2023-07-25 22:29:22 -0500

    completed

---
+++ src/main/java/org/haileab/lab2restfullwebservice/Lab2restFullWebServiceApplication.java
@@ -0,0 +1,14 @@
+package org.haileab.lab2restfullwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Lab2restFullWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2restFullWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/haileab/lab2restfullwebservice/backend/entity/Course.java
@@ -0,0 +1,12 @@
+package org.haileab.lab2restfullwebservice.backend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Course {
+    private String id;
+    private String name;
+    private String code;
+}
